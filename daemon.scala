aa97427a69d7ab6bf7bb068d71ef7c6400d7e750
Ken Mazaika <kmazaika@gmail.com>
2011-05-08 14:35:53 -0400

    start getting irc bot running; can connect, send message, exit channel.

--- daemon.scala
+++ daemon.scala
@@ -3,6 +3,16 @@ import akka.actor.Actor._
 import irc.actors._
 import irc.actors.util._
 
+import org.jibble.pircbot._;
+
+case class MyBot() extends PircBot {  
+
+def setup() = {
+  setName("pirc_ken_bot5");
+  }
+}
+
+
 // Run the IRC Bot Daemon
 object driver {
 
@@ -26,9 +36,34 @@ object driver {
 	}
 }
 
-driver.start
-driver("Make(actor)",        Make("actor"))
-driver("1 subordinate",     SubordinatesNames)
-driver("twitter actor should be hit", Ken("Kenneth Cole Reaction is a cool dude"))
-driver.stop
+//driver.start
+//driver("Make(actor)",        Make("actor"))
+//driver("1 subordinate",     SubordinatesNames)
+//driver("twitter actor should be hit", Ken("Kenneth Cole Reaction is a cool dude"))
+//driver.stop
+
+
+println("BOT STUFF")
+val bot = MyBot()
+
+println("my name: "+bot.getName());
+bot.setup()
+println("my name: "+bot.getName());
+
+// Enable debugging output.
+bot.setVerbose(true);
+
+// Connect to the IRC server.
+bot.connect("irc.freenode.net");
+
+// Join the #pircbot channel.
+bot.joinChannel("#pircbot");
+bot.sendMessage("#pircbot", "I just connected")
+println("my passowrd" + bot.getPassword())
+bot.partChannel("#pircbot");
+bot.quitServer("I quit!")
+
+
+
+
 
